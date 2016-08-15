9ae9982575f8ae86db3e320c2d81780e85b5019e
julius.blank <julius.blank@zeiss.com>
2016-08-15 12:51:00 +0200

    added setup for AWS CLI

--- digitalocean-dev/scripts/setup.sh
+++ digitalocean-dev/scripts/setup.sh
@@ -8,7 +8,6 @@
 # TODO Update this script according to the new 15.04 script from work         #
 # TODO Setup azure-cli (container with systemd-unit (systemd-docker)          #
 # TODO Setup mesosphere dcos                                                  #
-# TODO Setup AWS cli                                                          #
 # TODO Setup Google Cloud SDK                                                                       #
 ###############################################################################
 
@@ -62,4 +61,10 @@ export CLOUD_SDK_REPO=cloud-sdk-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+###############################################################################
+# Setup AWS CLI
+###############################################################################
+
+sudo pip install awscli
 
